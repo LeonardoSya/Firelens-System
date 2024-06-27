066e8314eee2ef092c997b2d674602134540a1b0
Zhang YiYang <120240967+LeonardoSya@users.noreply.github.com>
2024-06-27 14:22:33 +0800

    build: :arrow_up: upgrade to react 19 beta

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,17 +2,16 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
+    'plugin:react/recommended',
+    ' plugin:react/jsx-runtime',
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
     'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
   },
 }
