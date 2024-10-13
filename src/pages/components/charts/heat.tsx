aae989aa8e63f267be0cd8c6fcd2a460b894d75c
Zhang YiYang <120240967+LeonardoSya@users.noreply.github.com>
2024-10-13 14:13:36 +0800

    feat: 替换antv/l7为deck.gl
    替换antv/l7为deck.gl，目前使用mock数据

--- src/pages/components/charts/heat.tsx
+++ src/pages/components/charts/heat.tsx
@@ -1,74 +1,123 @@
-// import { useEffect, useRef } from 'react'
-// import { Scene, HeatmapLayer } from '@antv/l7'
-// import { Mapbox } from '@antv/l7-maps'
+import React, { useEffect, useState } from 'react'
+import { Map } from 'react-map-gl/maplibre'
+import { AmbientLight, PointLight, LightingEffect } from '@deck.gl/core'
+import { HexagonLayer } from '@deck.gl/aggregation-layers'
+import DeckGL from '@deck.gl/react'
+import { CSVLoader } from '@loaders.gl/csv'
+import { load } from '@loaders.gl/core'
+import type { Color, PickingInfo, MapViewState } from '@deck.gl/core'
 
-// const token = import.meta.env.VITE_MAPBOX_ACCESS_TOKEN
+// ! 数据使用自产ndvi 并修改ts类型定义
+const DATA_URL = '@/assets/data/heatmap-data.csv'
 
-// const HeatMap: React.FC = () => {
-//   const heatmapRef = useRef<HTMLDivElement>(null)
+// 环境光和两个点光源
+const ambientLight = new AmbientLight({
+  color: [255, 255, 255],
+  intensity: 1.0,
+})
 
-//   useEffect(() => {
-//     const scene = new Scene({
-//       id: heatmapRef.current,
-//       map: new Mapbox({
-//         style: 'mapbox://styles/mapbox/dark-v11',
-//         center: [120.13383079335335, 29.651873105004427],
-//         zoom: 7,
-//         pitch: 43,
-//         accessToken: token,
-//       }),
-//       logoVisible: false,
-//     })
+const pointLight1 = new PointLight({
+  color: [255, 255, 255],
+  intensity: 0.8,
+  position: [-0.144528, 49.739968, 80000],
+})
 
-//     scene.on('loaded', async () => {
-//       try {
-//         const res = await fetch(
-//           'https://gw.alipayobjects.com/os/basement_prod/a1a8158d-6fe3-424b-8e50-694ccf61c4d7.csv',
-//         )
-//         const data = await res.text()
-//         const layer = new HeatmapLayer({})
-//           .source(data, {
-//             parser: { type: 'csv', x: 'lng', y: 'lat' },
-//             transforms: [{ type: 'hexagon', size: 2500, field: 'v', method: 'sum' }],
-//           })
-//           .size('sum', (sum: number) => sum * 100)
-//           .shape('hexagonColumn')
-//           .style({ coverage: 0.8, angle: 0 })
-//           .color('sum', [
-//             '#094D4A',
-//             '#146968',
-//             '#1D7F7E',
-//             '#289899',
-//             '#34B6B7',
-//             '#4AC5AF',
-//             '#5FD3A6',
-//             '#7BE39E',
-//             '#A1EDB8',
-//             '#C3F9CC',
-//             '#DEFAC0',
-//             '#ECFFB1',
-//           ])
+const pointLight2 = new PointLight({
+  color: [255, 255, 255],
+  intensity: 0.8,
+  position: [-3.807751, 54.104682, 8000],
+})
 
-//         scene?.addLayer(layer)
-//       } catch (error) {
-//         console.error('加载数据出错: ', error)
-//       }
-//     })
+const lightingEffect = new LightingEffect({ ambientLight, pointLight1, pointLight2 })
 
-//     return () => {
-//       // if (sceneRef.current) {
-//       //   sceneRef.current.destroy()
-//       //   sceneRef.current = null
-//       // }
-//       scene.destroy()
-//     }
-//   }, [])
+// 初始视图状态
+const INITIAL_VIEW_STATE: MapViewState = {
+  longitude: -1.415727,
+  latitude: 52.232395,
+  zoom: 6.6,
+  minZoom: 5,
+  maxZoom: 15,
+  pitch: 40.5,
+  bearing: -27,
+}
 
-//   return (
-//     <div className='flex w-full items-center justify-center'>
-//       <div ref={heatmapRef} className='relative h-96 w-full md:h-[30rem] md:w-3/4' />
-//     </div>
-//   )
-// }
+const MAP_STYLE = 'https://basemaps.cartocdn.com/gl/dark-matter-nolabels-gl-style/style.json'
 
-// export default HeatMap
+export const colorRange: Color[] = [
+  [1, 152, 189],
+  [73, 227, 206],
+  [216, 254, 181],
+  [254, 237, 177],
+  [254, 173, 84],
+  [209, 55, 78],
+]
+
+function getTooltip({ object }: PickingInfo) {
+  if (!object) {
+    return null
+  }
+  const lat = object.position[1]
+  const lng = object.position[0]
+  const count = object.points.length
+
+  return `\
+    latitude: ${Number.isFinite(lat) ? lat.toFixed(6) : ''}
+    longitude: ${Number.isFinite(lng) ? lng.toFixed(6) : ''}
+    ${count} Accidents`
+}
+
+const HeatMap: React.FC = () => {
+  const [data, setData] = useState(null)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const loadedData = (await load(DATA_URL, CSVLoader)).data
+      // @ts-ignore
+      const points = loadedData.map(d => [d.lng, d.lat])
+      setData(points)
+    }
+    fetchData()
+  }, [])
+
+  // 创建热力图图层
+  const layers = [
+    new HexagonLayer<any>({
+      id: 'heatmap',
+      colorRange,
+      coverage: 1,
+      data,
+      elevationRange: [0, 3000],
+      elevationScale: data && data.length ? 50 : 0,
+      extruded: true,
+      getPosition: d => d,
+      pickable: true,
+      radius: 1000,
+      upperPercentile: 100,
+      material: {
+        ambient: 0.64,
+        diffuse: 0.6,
+        shininess: 32,
+        specularColor: [51, 51, 51],
+      },
+      transitions: {
+        elevationScale: 3000,
+      },
+    }),
+  ]
+
+  return (
+    <div className='relative h-96 w-full md:h-[36rem] md:w-3/4'>
+      <DeckGL
+        layers={layers}
+        effects={[lightingEffect]}
+        initialViewState={INITIAL_VIEW_STATE}
+        controller={true}
+        getTooltip={getTooltip}
+      >
+        <Map reuseMaps mapStyle={MAP_STYLE} />
+      </DeckGL>
+    </div>
+  )
+}
+
+export default HeatMap
